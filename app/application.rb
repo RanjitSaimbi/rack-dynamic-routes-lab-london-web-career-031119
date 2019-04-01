6972501fdea54807b98db46cd425c337a7374858
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-04-01 14:41:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,13 @@
 class Application 
   
  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
+  resp = Rack::Response.new
+  req = Rack::Request.new(env)
    
    
    
    
-   resp.finish 
+  resp.finish 
  end 
   
   
