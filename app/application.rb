fee70b2c3c1b94a3c090c72d5ed6fc741f299a78
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-04-01 15:04:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
       if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
       else resp.write 
+        resp.write "Route not found"
         resp.status = 400
       end 
     else 
