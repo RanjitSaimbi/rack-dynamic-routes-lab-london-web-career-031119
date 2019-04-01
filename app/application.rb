dab6e44b63e3ff88eea60183dd76f3a45d654007
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-04-01 15:02:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,10 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
+        resp.write item.price
+      else 
+        
+      end 
       
     end
   resp.finish 
