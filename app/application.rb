257b76fd6413ce931c6a6034f2b7e77a7cb8ad72
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-04-01 15:05:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,11 +10,12 @@ class Application
       if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
       else resp.write 
-        resp.write "Route not found"
+        resp.write "Item not found"
         resp.status = 400
       end 
     else 
       resp.status = 404
+      resp.write "Route not found"
     end
   resp.finish 
  end 
